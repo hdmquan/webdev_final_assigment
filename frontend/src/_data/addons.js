62b6b93c3205f915e733923e0b1d1236d91d5eb8
Alan Huynh <hdmquan@outlook.com>
2025-05-17 19:45:50 +1000

    Initial commit

---
+++ frontend/src/_data/addons.js
@@ -0,0 +1,48 @@
+module.exports = [
+  {
+    name: "Extra Pages",
+    price: 99,
+    unit: "/ page",
+    features: [
+      "Add extra pages to your package with a one-time fee.\nEach page includes our Green Pagespeed Guarantee and Unlimited Edits."
+    ]
+  },
+  {
+    name: "Hosting",
+    price: 39,
+    unit: "/ month",
+    features: [
+      "Hosting for 'lump sum' plans is available at an extra cost.\nIt features fast performance, 99.98% uptime, free SSL, and unlimited backups."
+    ]
+  },
+  {
+    name: "Blog Integration",
+    price: 299,
+    features: [
+      "Enhance your site with blog capability for SEO and engagement.\nIncludes setup, but not ongoing posts. Ideal for full content control."
+    ]
+  },
+  {
+    name: "Logos & Branding",
+    from: true,
+    price: 149,
+    features: [
+      "Get a custom logo representing your brand.\nIncludes PNG, SVG, and source files with 4 concepts and 6 revisions."
+    ]
+  },
+  {
+    name: "Custom Illustrations",
+    price: 79,
+    features: [
+      "Unique, professional illustrations tailored to your project.\nPerfect for menus, promotions, or any custom design need."
+    ]
+  },
+  {
+    name: "AdvancedCopywriting",
+    price: 699,
+    unit: "/ 1000 words",
+    features: [
+      "Engaging, SEO-optimized copy that connects with your audience provided by a professional copywriter.\nIdeal for websites, product descriptions, or marketing material."
+    ]
+  }
+];
---
+++ frontend/src/_data/pricing.js
@@ -0,0 +1,80 @@
+module.exports = {
+    monthly: [
+      {
+        plan: "Basic Plan",
+        price: "$199",
+        description: "Perfect for small businesses looking to establish a professional online presence.",
+        features: [
+          "Custom-coded website with up to 5 pages",
+          "Essential on-page SEO with local focus",
+          "Help setup Google Business Profile",
+          "Full analytics integration",
+          "Free logo SVG conversion",
+          "24/7 on-call support",
+          "1 Additional feature (online booking etc.)"
+        ]
+      },
+      {
+        plan: "Professional Plan",
+        price: "$479",
+        description: "Ideal for growing businesses needing advanced features and enhanced SEO.",
+        features: [
+          "All features in the Basic Plan",
+          "Custom-coded website with up to 10 pages",
+          "Advanced SEO & local backlinking",
+          "Free logo (re)design and/or SVG conversion",
+          "Email marketing integration",
+          "Integrated blog setup",
+          // "1 custom blog post per month"
+        ]
+      },
+      {
+        plan: "Premium Plan",
+        price: "$1299",
+        description: "Comprehensive package for businesses ready to dominate their market.",
+        features: [
+          "All features in the Professional Plan",
+          "Fully custom-coded website with up to 30 pages",
+          "Extensive SEO & ongoing backlinking strategy",
+          "CRM & email marketing integration",
+          "Monthly high-detail illustrations",
+          "1 custom blog post per month"
+        ]
+      }
+    ],
+    oneOff: [
+      {
+        plan: "Standard Plan",
+        price: "$5499",
+        description: "A custom website with up to 10 pages, local SEO, an integrated blog, a free logo redesign, and top-notch performance optimization.",
+        features: [
+            "Custom-coded website with up to 10 pages.",
+            "Essential on-page SEO with a local focus.",
+            "Integrated blog setup.",
+            "Free logo (re)design and/or SVG conversion."
+        ]
+      },
+      {
+        plan: "E-Commerce Plan",
+        price: "$9499",
+        description: "All the features of the Standard Plan plus full e-commerce functionality, including product catalog setup, payment gateway integration, and basic product SEO.",
+        features: [
+          "Includes all features of the Standard Plan.",
+            "E-commerce integration (WooCommerce or Shopify).",
+            "Product catalog setup for up to 50 products.",
+            "Payment gateway integration.",
+            "Shopping cart and checkout setup.",
+            "Product SEO (title tags, meta descriptions, schema markup).",
+        ]
+      },
+      {
+        plan: "SaaS Plan",
+        price: "Contact Us",
+        description: "A complete SaaS solution with user authentication, subscription management, advanced customizations, and API integrations. Build with Bubble.io.",
+        features: [
+            "Contact Us for more information"
+        ]
+      }
+    ]
+  };
+  
